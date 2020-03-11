09ff92b7c443ee4ce7c9b69dfac981e7339fec2a
nogawanogawa <t_tsuno_tsuno@yahoo.co.jp>
2020-03-11 20:19:44 +0900

    [add] wordcloud

--- src/components/templates/RegisterPanel.vue
+++ src/components/templates/RegisterPanel.vue
@@ -2,7 +2,7 @@
   <v-card>
     <v-card-title class="title">WordCloud</v-card-title>
     <div id="app">
-      <wordcloud :data="words" nameKey="name" valueKey="value" color="Accent"></wordcloud>
+      <wordcloud :data="res" nameKey="name" valueKey="value" color="Accent"></wordcloud>
     </div>
   </v-card>
 </template>
@@ -20,67 +20,11 @@ export default {
   data() {
     return {
       endpoint: config.route + "twitter_word_cloud",
-      words: [
-        {
-          name: "猫",
-          value: 26
-        },
-        {
-          name: "猫",
-          value: 26
-        },
-        {
-          name: "猫",
-          value: 26
-        },
-        {
-          name: "猫",
-          value: 26
-        },
-        {
-          name: "猫",
-          value: 26
-        },
-        {
-          name: "魚",
-          value: 19
-        },
-        {
-          name: "things",
-          value: 18
-        },
-        {
-          name: "look",
-          value: 16
-        },
-        {
-          name: "two",
-          value: 15
-        },
-        {
-          name: "fun",
-          value: 9
-        },
-        {
-          name: "know",
-          value: 9
-        },
-        {
-          name: "good",
-          value: 9
-        },
-        {
-          name: "play",
-          value: 6
-        }
-      ]
+      res: []
     };
   },
-  methods: {},
-  mounted: function() {
-    axios
-      .get(this.endpoint)
-      .then(response => (this.words = response.data.words));
+  mounted() {
+    axios.get(this.endpoint).then(response => (this.res = response.data.words));
   }
 };
 </script>
